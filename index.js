0513bef61d6e939ee0b0a7108a3abefce28a7c6f
Kanon-Hosen <freelancerkanon1@gmail.com>
2022-11-24 00:35:35 +0600

    Server Create

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 const express = require('express');
 const cors = require('cors');
 require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
 
 const app = express();
 const port = process.env.PORT || 4000;
 app.use(cors());
 app.use(express.json());
 
-
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASS}@cluster0.we2oxi5.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const dbConnect = () => {
+    try {
+        client.connect();
+    } catch (error) {
+        console.log(error.message)
+    }
+}
+dbConnect()
 
 // ! Server Start:::::::::::::::
 app.get('/', (req, res) => {
